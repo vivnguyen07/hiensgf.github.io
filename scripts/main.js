0d940ba7b33cb28469e48279b3212cd66e477960
Vivian <94405198+vivnguyen07@users.noreply.github.com>
2023-11-04 19:00:39 -0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const novelText = document.getElementById('novel-text');
+const choice1 = document.getElementById('choice-1');
+const choice2 = document.getElementById('choice-2');
+
+// Initial game state
+let storyIndex = 0;
+
+const story = [
+    { text: "Page 1 text", choices: ["Choice 1", "Choice 2"] },
+    { text: "Page 2 text", choices: ["Choice 3", "Choice 4"] },
+    // Add more pages and choices as needed
+];
+
+function updatePage() {
+    if (storyIndex < story.length) {
+        novelText.textContent = story[storyIndex].text;
+        choice1.textContent = story[storyIndex].choices[0];
+        choice2.textContent = story[storyIndex].choices[1];
+    } else {
+      print(I love you Happy Anniversary)
+        // End of the story, you can add a "The End" message or link to restart.
+    }
+}
+
+choice1.addEventListener('click', () => {
+    storyIndex++;
+    updatePage();
+});
+
+choice2.addEventListener('click', () => {
+    storyIndex++;
+    updatePage();
+});
+
+// Start the visual novel
+updatePage();
